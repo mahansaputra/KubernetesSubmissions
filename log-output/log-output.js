2c2c74b88bae9972af90eda66cc7938d5b6fb98e
mahansaputra <mahansaputra@gmail.com>
2025-08-05 01:28:46 +0700

    added configmap to log-output and also change the output of log-output.js

--- log-output/log-output.js
+++ log-output/log-output.js
@@ -1,11 +1,12 @@
 const http = require('http');
 const axios = require('axios');
 const { v4: uuidv4 } = require('uuid');
+const fs = require('fs');
 
 const logOutputPort = 80;
 const pingPongUrl = 'http://pingpong-service:80/pingpong';
 
-let currentUuid = uuidv4(); // Initial UUID
+let currentUuid = uuidv4();
 
 // Update UUID every 5 seconds
 setInterval(() => {
@@ -18,11 +19,13 @@ const server = http.createServer(async (req, res) => {
             const response = await axios.get(pingPongUrl);
             const pongs = response.data.pongs;
             const timestamp = new Date().toISOString();
+            const fileContent = fs.readFileSync('/app/config/information.txt', 'utf8');
+            const envMessage = process.env.MESSAGE || 'default message';
             res.writeHead(200, { 'Content-Type': 'text/plain' });
-            res.end(`${timestamp}: ${currentUuid}.\nPing / Pongs: ${pongs}`);
+            res.end(`${timestamp}: ${currentUuid}.\nPing / Pongs: ${pongs}\nEnv Message: ${envMessage}\nFile Content: ${fileContent}`);
         } catch (error) {
             res.writeHead(500, { 'Content-Type': 'text/plain' });
-            res.end('Error fetching pongs');
+            res.end('Error fetching data');
         }
     } else {
         res.writeHead(404, { 'Content-Type': 'text/plain' });
