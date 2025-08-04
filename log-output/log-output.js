379f2ba3d5b0ae823d00801162b9695c82f351cb
mahansaputra <mahansaputra@gmail.com>
2025-08-04 17:52:53 +0700

    Refactor log-output and pingpong applications: update UUID handling, modify imagePullPolicy to Never, and add uuid dependency.

--- log-output/log-output.js
+++ log-output/log-output.js
@@ -1,8 +1,16 @@
 const http = require('http');
 const axios = require('axios');
+const { v4: uuidv4 } = require('uuid');
 
 const logOutputPort = 80;
 const pingPongUrl = 'http://pingpong-service:80/pingpong';
+
+let currentUuid = uuidv4(); // Initial UUID
+
+// Update UUID every 5 seconds
+setInterval(() => {
+    currentUuid = uuidv4();
+}, 5000);
 
 const server = http.createServer(async (req, res) => {
     if (req.method === 'GET' && req.url === '/') {
@@ -10,9 +18,8 @@ const server = http.createServer(async (req, res) => {
             const response = await axios.get(pingPongUrl);
             const pongs = response.data.pongs;
             const timestamp = new Date().toISOString();
-            const uuid = '8523ecb1-c716-4cb6-a044-b9e83bb98e43';
             res.writeHead(200, { 'Content-Type': 'text/plain' });
-            res.end(`${timestamp}: ${uuid}.\nPing / Pongs: ${pongs}`);
+            res.end(`${timestamp}: ${currentUuid}.\nPing / Pongs: ${pongs}`);
         } catch (error) {
             res.writeHead(500, { 'Content-Type': 'text/plain' });
             res.end('Error fetching pongs');
