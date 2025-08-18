b8511531968ede80082f18eb493714aea6909482
Hansa <mahansaputra@gmail.com>
2025-08-18 22:00:25 +0700

    added terraform file to apply the gke cluster

---
+++ terraform-gke/main.tf
@@ -0,0 +1,39 @@
+# Configuring the Google Cloud provider
+provider "google" {
+  project = var.project_id
+  region  = "asia-southeast2"
+  zone    = "asia-southeast2-a"
+}
+
+# Defining the GKE cluster resource
+resource "google_container_cluster" "dwk_cluster" {
+  name     = "dwk-cluster"
+  location = "asia-southeast2-a"
+
+  # Setting the GKE version
+  release_channel {
+    channel = "SPECIFIC"
+  }
+  min_master_version = "1.32"
+
+  # Configuring the node pool
+  node_pool {
+    name       = "default-pool"
+    node_count = 3
+
+    node_config {
+      machine_type = "e2-micro"
+      disk_size_gb = 32
+      disk_type    = "pd-standard"
+    }
+  }
+
+  # Removing default node pool after creation to avoid conflicts
+  remove_default_node_pool = false
+}
+
+# Defining variables for project ID
+variable "project_id" {
+  description = "The ID of the Google Cloud project"
+  type        = string
+}
